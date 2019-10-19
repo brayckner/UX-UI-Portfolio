e620b57165254f93855f2681dc273218d8932b65
Carlos Rincon <mezerotm@gmail.com>
2019-10-19 14:52:51 -0400

    Hopefully Fixed Forms

--- src/pages/Contact/Index.vue
+++ src/pages/Contact/Index.vue
@@ -1,24 +1,28 @@
 <template lang="pug">
     Layout.container.mx-auto
-        flex.flex.justify-center.my-20
-            form.flex.flex-col(class="w-1/2" name="contact" method="post" v-on:submit.prevent="handleSubmit" action="/contact/success/" data-netlify="true" data-netlify-honeypot="bot-field")
+        div.flex.justify-center.my-20
+            form.flex.flex-col(class="w-1/2" name='contact', method='post', v-on:submit.prevent='handleSubmit', action='/success/', data-netlify='true', data-netlify-honeypot='bot-field')
                 h1.font-bold.text-gray-500.leading-loose.text-xl Lets start a conversation
-                input(type="hidden" name="form-name" value="contact")
-                p(hidden)
+
+                input(type='hidden', name='form-name', value='contact')
+                p(hidden='')
                     label
-                        input(name="bot-field")
-                input.shadow-md.rounded-lg.my-2.p-2.border.border-gray-600(type="text" placeholder="Name" v-model="formData.name" required)
-                input.shadow-md.rounded-lg.my-2.p-2.border.border-gray-600(type="email" placeholder="Your email address" v-model="formData.email" required)
-                textarea.shadow-md.rounded-lg.my-2.p-2.border.border-gray-600.h-20(type="text" placeholder="Message" v-model="formData.message" required)
+                        | Don&rsquo;t fill this out:
+                        input(name='bot-field')
+
+                input.shadow-md.rounded-lg.my-2.p-2.border.border-gray-600(id="name", type='text', name='name', v-model='formData.name' placeholder="Name" required)
+                input.shadow-md.rounded-lg.my-2.p-2.border.border-gray-600(id="email", type='email', name='email', v-model='formData.email' placeholder="Your email address" required)
+
+                textarea.shadow-md.rounded-lg.my-2.p-2.border.border-gray-600(id="message", name='message', v-model='formData.message' placeholder="Message" required)
                 div.flex.justify-end
-                    button.shadow-md.rounded-full.my-2.p-4.bg-white.font-bold(class="w-2/5" type="submit") Submit
+                    button.shadow-md.rounded-full.my-2.p-4.bg-white.font-bold(type='submit') Submit form
 </template>
 
 <script>
     export default {
-      data() {
+      data () {
         return {
-          formData: {},
+          formData: {}
         }
       },
       methods: {
@@ -36,7 +40,7 @@
               ...this.formData,
             }),
           })
-          .then(() => this.$router.push('/contact/success'))
+          .then(() => this.$router.push('/success'))
           .catch(error => alert(error))
         }
       }
